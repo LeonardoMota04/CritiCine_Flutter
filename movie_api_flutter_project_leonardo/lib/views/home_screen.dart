2e1bad138c26bda06700c23b8fec87e5eac2c04d
Leonardo Pereira Mota <leomotadf@gmail.com>
2025-05-19 21:02:47 -0300

    Added one haptic feedback in grid button

--- movie_api_flutter_project_leonardo/lib/views/home_screen.dart
+++ movie_api_flutter_project_leonardo/lib/views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../viewmodels/movie_viewmodel.dart';
 import '../viewmodels/theme_viewmodel.dart';
@@ -171,6 +172,7 @@ class HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMi
             setState(() {
               _isGridView = !_isGridView;
             });
+            HapticFeedback.lightImpact();
           },
         ),
       ],
